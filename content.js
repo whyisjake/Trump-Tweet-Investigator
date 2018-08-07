abc274fc15752be92f6e0a5c6388522f0d149f12
Jake Spurlock <whyisjake@gmail.com>
2018-08-07 11:11:57 -0700

    Make things Prettier.

--- content.js
+++ content.js
@@ -1,8 +1,7 @@
 // Let's make Trump a little more accountable for his Tweets.
 
 // Append the button to the action bar.
-$('[data-screen-name~="realDonaldTrump"]')
-  .find(".ProfileTweet-actionList")
+$('[data-screen-name~="realDonaldTrump"]').find(".ProfileTweet-actionList")
   .append(`
     <div class="ProfileTweet-action ProfileTweet-action--search">
       <a target="_blank" class="ProfileTweet-actionButton u-textUserColorHover js-actionButton js-actionShareViasearch validator" type="button" data-nav="share_tweet_search" href="http://google.com">
@@ -20,20 +19,26 @@ $('[data-screen-name~="realDonaldTrump"]')
 
 // Add the stripes.
 $('[data-screen-name~="realDonaldTrump"]')
-  .closest('.tweet')
-  .css({"background-color": "white", margin: "5px"})
-  .closest('li')
+  .closest(".tweet")
+  .css({ "background-color": "white", margin: "5px" })
+  .closest("li")
   .addClass("liar-stripes");
 
 // Update the link
-$('[data-screen-name~="realDonaldTrump"]').each(function(){
+$('[data-screen-name~="realDonaldTrump"]').each(function() {
   let data = $(this).data();
-  const tweetText = $(this).find(".js-tweet-text-container").text().replace(/(\r\n\t|\n|\r\t)/gm, "").trim();
+  const tweetText = $(this)
+    .find(".js-tweet-text-container")
+    .text()
+    .replace(/(\r\n\t|\n|\r\t)/gm, "")
+    .trim();
   $(this)
     .find(".validator")
-    .attr("href", `http://www.google.com/search?q=${encodeURI(tweetText)}&ie=utf-8&oe=utf-8`);
-})
-
+    .attr(
+      "href",
+      `http://www.google.com/search?q=${encodeURI(tweetText)}&ie=utf-8&oe=utf-8`
+    );
+});
 
 // TODO: Add better search params.
 
